dda2dea30018e3783865672873907e16b5ca4f8d
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-05-20 22:03:40 +0630

    Update tgbot.go

--- web/service/tgbot.go
+++ web/service/tgbot.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/alireza0/x-ui/config"
-	"github.com/alireza0/x-ui/database"
-	"github.com/alireza0/x-ui/database/model"
-	"github.com/alireza0/x-ui/logger"
-	"github.com/alireza0/x-ui/util/common"
-	"github.com/alireza0/x-ui/web/locale"
-	"github.com/alireza0/x-ui/xray"
+	"github.com/nyeinkokoaung404/x-ui/config"
+	"github.com/nyeinkokoaung404/x-ui/database"
+	"github.com/nyeinkokoaung404/x-ui/database/model"
+	"github.com/nyeinkokoaung404/x-ui/logger"
+	"github.com/nyeinkokoaung404/x-ui/util/common"
+	"github.com/nyeinkokoaung404/x-ui/web/locale"
+	"github.com/nyeinkokoaung404/x-ui/xray"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
