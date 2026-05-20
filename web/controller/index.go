87fefaaf94bf899f66fa479f012bf82361cfd34f
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-05-20 22:10:14 +0630

    Update index.go

--- web/controller/index.go
+++ web/controller/index.go
@@ -5,9 +5,9 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/alireza0/x-ui/logger"
-	"github.com/alireza0/x-ui/web/service"
-	"github.com/alireza0/x-ui/web/session"
+	"github.com/nyeinkokoaung404/x-ui/logger"
+	"github.com/nyeinkokoaung404/x-ui/web/service"
+	"github.com/nyeinkokoaung404/x-ui/web/session"
 
 	"github.com/gin-gonic/gin"
 )
