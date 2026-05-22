d20fabb5401cf7c56c304b1c1159de9ce481fb26
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-05-23 04:17:08 +0630

    fix session maxAge #1625

--- web/assets/js/langs.js
+++ web/assets/js/langs.js
@@ -5,6 +5,11 @@ const supportLangs = [
         icon: '🇺🇸',
     },
     {
+        name: 'မြန်မာဘာသာ',
+        value: 'my-MM',
+        icon: '🇲🇲',
+    },
+    {
         name: 'فارسی',
         value: 'fa-IR',
         icon: '🇮🇷',
