8738cc45346236dadfc2fa27e8a7b605f675e15c
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-18 09:12:37 +0630

    [feat] IP limit

--- database/model/model.go
+++ database/model/model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/nyeinkokoaung404/x-ui/util/json_util"
@@ -46,6 +47,69 @@ type Inbound struct {
 	Sniffing       string   `json:"sniffing" form:"sniffing"`
 }
 
+type Outbound struct {
+	Id             int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	Up             int64  `json:"up" form:"up"`
+	Down           int64  `json:"down" form:"down"`
+	Sort           int    `json:"sort" form:"sort"`
+	SendThrough    string `json:"sendThrough" form:"sendThrough"`
+	Protocol       string `json:"protocol" form:"protocol"`
+	Settings       string `json:"settings" form:"settings"`
+	Tag            string `json:"tag" form:"tag" gorm:"unique"`
+	StreamSettings string `json:"streamSettings" form:"streamSettings"`
+	ProxySettings  string `json:"proxySettings" form:"proxySettings"`
+	Mux            string `json:"mux" form:"mux"`
+	TargetStrategy string `json:"targetStrategy" form:"targetStrategy"`
+}
+
+type RoutingRule struct {
+	Id      int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	Tag     string `json:"tag" form:"tag" gorm:"unique"`
+	Sort    int    `json:"sort" form:"sort"`
+	RawJson string `json:"rawJson" form:"rawJson"`
+}
+
+func (r *RoutingRule) RuleJSON() ([]byte, error) {
+	var raw map[string]interface{}
+	if len(r.RawJson) > 0 {
+		if err := json.Unmarshal([]byte(r.RawJson), &raw); err != nil {
+			return nil, err
+		}
+	} else {
+		raw = map[string]interface{}{"type": "field"}
+	}
+	if r.Tag != "" {
+		raw["ruleTag"] = r.Tag
+	}
+	return json.Marshal(raw)
+}
+
+func (o *Outbound) GenXrayOutboundConfig() *xray.OutboundConfig {
+	cfg := &xray.OutboundConfig{
+		Protocol:       o.Protocol,
+		Tag:            o.Tag,
+		TargetStrategy: o.TargetStrategy,
+	}
+	if o.SendThrough != "" {
+		cfg.SendThrough = json_util.RawMessage(fmt.Sprintf("\"%s\"", o.SendThrough))
+	}
+	if len(o.Settings) > 0 {
+		cfg.Settings = json_util.RawMessage(o.Settings)
+	} else {
+		cfg.Settings = json_util.RawMessage("{}")
+	}
+	if len(o.StreamSettings) > 0 {
+		cfg.StreamSettings = json_util.RawMessage(o.StreamSettings)
+	}
+	if len(o.ProxySettings) > 0 {
+		cfg.ProxySettings = json_util.RawMessage(o.ProxySettings)
+	}
+	if len(o.Mux) > 0 {
+		cfg.Mux = json_util.RawMessage(o.Mux)
+	}
+	return cfg
+}
+
 func (i *Inbound) GenXrayInboundConfig() *xray.InboundConfig {
 	listen := i.Listen
 	if listen != "" {
@@ -69,17 +133,18 @@ type Setting struct {
 }
 
 type ClientReverse struct {
-	Tag      string `json:"tag"`
-	Sniffing string `json:"sniffing" omitEmpty:"true"`
+	Tag      string               `json:"tag"`
+	Sniffing json_util.RawMessage `json:"sniffing,omitempty"`
 }
 type Client struct {
-	ID         string         `json:"id" omitEmpty:"true"`
-	Password   string         `json:"password" omitEmpty:"true"`
-	Auth       string         `json:"auth" omitEmpty:"true"`
-	Flow       string         `json:"flow" omitEmpty:"true"`
-	Reverse    *ClientReverse `json:"reverse" omitEmpty:"true"`
+	ID         string         `json:"id,omitempty"`
+	Password   string         `json:"password,omitempty"`
+	Auth       string         `json:"auth,omitempty"`
+	Flow       string         `json:"flow,omitempty"`
+	Reverse    *ClientReverse `json:"reverse,omitempty"`
 	Email      string         `json:"email"`
 	TotalGB    int64          `json:"totalGB" form:"totalGB"`
+	LimitIP    uint16         `json:"limitIp" form:"limitIp"`
 	ExpiryTime int64          `json:"expiryTime" form:"expiryTime"`
 	Enable     bool           `json:"enable" form:"enable"`
 	TgID       string         `json:"tgId" form:"tgId"`
