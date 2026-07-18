084206ccc394b776f496176e77aecc1982ba2d25
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-18 09:18:30 +0630

    hy link enhancements #1635

--- sub/subService.go
+++ sub/subService.go
@@ -262,6 +262,14 @@ func (s *SubService) genVmessLink(inbound *model.Inbound, email string) string {
 			if insecure, ok := searchKey(tlsSettings, "allowInsecure"); ok {
 				obj["allowInsecure"], _ = insecure.(bool)
 			}
+			if pcs := pinnedPeerCertSha256ToString(tlsSettings); pcs != "" {
+				obj["pcs"] = pcs
+			}
+			if vcn, ok := searchKey(tlsSettings, "verifyPeerCertByName"); ok {
+				if vcnStr, _ := vcn.(string); vcnStr != "" {
+					obj["vcn"] = vcnStr
+				}
+			}
 		}
 	}
 
@@ -336,7 +344,7 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 	if inbound.Protocol != model.VLESS {
 		return ""
 	}
-	var vlessSettings model.VLESSSettings
+	var vlessSettings map[string]interface{}
 	_ = json.Unmarshal([]byte(inbound.Settings), &vlessSettings)
 
 	var stream map[string]interface{}
@@ -353,8 +361,8 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 	port := inbound.Port
 	streamNetwork := stream["network"].(string)
 	params := make(map[string]string)
-	if vlessSettings.Encryption != "" {
-		params["encryption"] = vlessSettings.Encryption
+	if vlessEncryption, ok := vlessSettings["encryption"].(string); ok && vlessEncryption != "" {
+		params["encryption"] = vlessEncryption
 	}
 	params["type"] = streamNetwork
 
@@ -446,6 +454,14 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 			if insecure, ok := searchKey(tlsSettings, "allowInsecure"); ok {
 				if insecure.(bool) {
 					params["allowInsecure"] = "1"
+				}
+			}
+			if pcs := pinnedPeerCertSha256ToString(tlsSettings); pcs != "" {
+				params["pcs"] = pcs
+			}
+			if vcn, ok := searchKey(tlsSettings, "verifyPeerCertByName"); ok {
+				if vcnStr, _ := vcn.(string); vcnStr != "" {
+					params["vcn"] = vcnStr
 				}
 			}
 		}
@@ -678,6 +694,14 @@ func (s *SubService) genTrojanLink(inbound *model.Inbound, email string) string 
 					params["allowInsecure"] = "1"
 				}
 			}
+			if pcs := pinnedPeerCertSha256ToString(tlsSettings); pcs != "" {
+				params["pcs"] = pcs
+			}
+			if vcn, ok := searchKey(tlsSettings, "verifyPeerCertByName"); ok {
+				if vcnStr, _ := vcn.(string); vcnStr != "" {
+					params["vcn"] = vcnStr
+				}
+			}
 		}
 	}
 
@@ -905,6 +929,14 @@ func (s *SubService) genShadowsocksLink(inbound *model.Inbound, email string) st
 					params["allowInsecure"] = "1"
 				}
 			}
+			if pcs := pinnedPeerCertSha256ToString(tlsSettings); pcs != "" {
+				params["pcs"] = pcs
+			}
+			if vcn, ok := searchKey(tlsSettings, "verifyPeerCertByName"); ok {
+				if vcnStr, _ := vcn.(string); vcnStr != "" {
+					params["vcn"] = vcnStr
+				}
+			}
 		}
 	}
 
@@ -1038,6 +1070,61 @@ func (s *SubService) genHysteriaLink(inbound *model.Inbound, email string) strin
 	protocol := "hysteria2"
 	if int(version) == 1 {
 		protocol = "hysteria"
+	}
+
+	if fm, ok := stream["finalmask"].(map[string]interface{}); ok {
+		if qp, ok := fm["quicParams"].(map[string]interface{}); ok {
+			if v, ok := qp["congestion"].(string); ok && v != "" {
+				params["congestion"] = v
+			}
+			if v, ok := qp["brutalUp"].(string); ok && v != "" {
+				params["up"] = v
+			}
+			if v, ok := qp["brutalDown"].(string); ok && v != "" {
+				params["down"] = v
+			}
+			if udpHop, ok := qp["udpHop"].(map[string]interface{}); ok {
+				if v, ok := udpHop["ports"].(string); ok && v != "" {
+					params["mport"] = v
+				}
+				switch iv := udpHop["interval"].(type) {
+				case string:
+					if iv != "" {
+						params["udphopInterval"] = iv
+					}
+				case float64:
+					params["udphopInterval"] = fmt.Sprintf("%d", int(iv))
+				}
+			}
+			for jsonKey, paramKey := range map[string]string{
+				"initStreamReceiveWindow":     "initStreamReceiveWindow",
+				"maxStreamReceiveWindow":      "maxStreamReceiveWindow",
+				"initConnectionReceiveWindow": "initConnectionReceiveWindow",
+				"maxConnectionReceiveWindow":  "maxConnectionReceiveWindow",
+				"maxIdleTimeout":              "maxIdleTimeout",
+				"keepAlivePeriod":             "keepAlivePeriod",
+			} {
+				if v, ok := qp[jsonKey].(float64); ok && v != 0 {
+					params[paramKey] = fmt.Sprintf("%d", int(v))
+				}
+			}
+			if v, ok := qp["disablePathMTUDiscovery"].(bool); ok && v {
+				params["disablePathMTUDiscovery"] = "true"
+			}
+		}
+		if udpMasks, ok := fm["udp"].([]interface{}); ok {
+			for _, m := range udpMasks {
+				mask, _ := m.(map[string]interface{})
+				settings, _ := mask["settings"].(map[string]interface{})
+				password, _ := settings["password"].(string)
+				maskType, _ := mask["type"].(string)
+				if password != "" && maskType != "" {
+					params["obfs"] = maskType
+					params["obfs-password"] = password
+					break
+				}
+			}
+		}
 	}
 
 	externalProxies, _ := stream["externalProxy"].([]interface{})
@@ -1284,6 +1371,26 @@ func searchKey(data interface{}, key string) (interface{}, bool) {
 		}
 	}
 	return nil, false
+}
+
+func pinnedPeerCertSha256ToString(tlsSettings interface{}) string {
+	pcsValue, ok := searchKey(tlsSettings, "pinnedPeerCertSha256")
+	if !ok {
+		return ""
+	}
+	switch v := pcsValue.(type) {
+	case []interface{}:
+		var pcs []string
+		for _, h := range v {
+			if hs, ok := h.(string); ok && len(hs) > 0 {
+				pcs = append(pcs, hs)
+			}
+		}
+		return strings.Join(pcs, ",")
+	case string:
+		return v
+	}
+	return ""
 }
 
 func searchHost(headers interface{}) string {
